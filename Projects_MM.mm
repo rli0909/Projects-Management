cf5bfb3040eeb98a1c5379d82a87e47ee93bd6eb
rli0909 <lucywou12@gmail.com>
2018-07-18 10:51:18 -0700

    7/18/2018 Rename Notes_MM.mm => Projects_MM.mm

--- Projects_MM.mm
+++ Projects_MM.mm
@@ -1,8 +1,10 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1531932074564" ID="ID_442726086" MODIFIED="1531932477353" TEXT="Projects">
-<node CREATED="1531932491266" ID="ID_637021527" MODIFIED="1531932869199" POSITION="right" TEXT="UCS">
-<node CREATED="1531932508460" ID="ID_466957248" MODIFIED="1531932570638" TEXT="Secondary Email Request Form"/>
+<node CREATED="1531932491266" FOLDED="true" ID="ID_637021527" MODIFIED="1531936160939" POSITION="right" TEXT="UCS(On going)">
+<node CREATED="1531932508460" ID="ID_466957248" MODIFIED="1531936108642" TEXT="Secondary Email Request Form">
+<icon BUILTIN="full-2"/>
+</node>
 <node CREATED="1531932583158" ID="ID_700880179" MODIFIED="1531932644946" TEXT="WOU Budget Proposal (Due by end of July)">
 <icon BUILTIN="full-1"/>
 </node>
