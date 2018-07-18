71eb50fd4e02cc823e6c652e89f0f4fca1752541
rli0909 <lucywou12@gmail.com>
2018-07-18 10:44:08 -0700

    7/18/2018 Add Notes_MM.mm Mind Map for Projects

---
+++ Notes_MM.mm
@@ -0,0 +1,12 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1531932074564" ID="ID_442726086" MODIFIED="1531932477353" TEXT="Projects">
+<node CREATED="1531932491266" ID="ID_637021527" MODIFIED="1531932869199" POSITION="right" TEXT="UCS">
+<node CREATED="1531932508460" ID="ID_466957248" MODIFIED="1531932570638" TEXT="Secondary Email Request Form"/>
+<node CREATED="1531932583158" ID="ID_700880179" MODIFIED="1531932644946" TEXT="WOU Budget Proposal (Due by end of July)">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1531932890344" ID="ID_1207894790" MODIFIED="1531932890344" TEXT=""/>
+</node>
+</node>
+</map>
